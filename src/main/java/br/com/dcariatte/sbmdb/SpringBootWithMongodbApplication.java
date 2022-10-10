5ff5cad739d774c27937b56ec42c2dfe3ae359ba
Cariatte <nielcariatte@gmail.com>
2022-10-10 12:04:57 -0300

    projeto criado

---
+++ src/main/java/br/com/dcariatte/sbmdb/SpringBootWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.dcariatte.sbmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dcariatte/sbmdb/SpringBootWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dcariatte.sbmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
